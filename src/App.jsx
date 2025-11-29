934501480139a308776f5040fb44d799a6c40ac5
ye-seob <jakalob103@naver.com>
2025-11-29 21:02:30 +0900

    프론트 개발 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div className="App">
+            <div >
+                <h1>서경대 수강신청 시뮬레이터</h1>
+            </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+            <div >
+                <p>여기에 페이지 내용을 추가하세요.</p>
+            </div>
 
-export default App
+            <div>
+                © 2025 SKU Course Simulator
+            </div>
+        </div>
+    );
+};
+
+export default App;
