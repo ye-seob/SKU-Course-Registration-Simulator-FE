594b114f8a693174e122c56e43a1378114d26829
ye-seob <jakalob103@naver.com>
2025-11-30 17:35:26 +0900

    :sparkles: feat(#3): 로그인 페이지 UI 구현

---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import '../styles/login.css';
+
+const logoUrl = "https://s.skuniv.ac.kr/course/img/sugang/logo2.gif";
+const loginBtnUrl = "https://s.skuniv.ac.kr/course/img/sugang/login2.gif"
+
+function Login() {
+    return (
+        // 전체를 감싸는 컨테이너
+        <div className="login-container">
+            <header className="login-header">
+                <div className="logo-area">
+                    <img src={logoUrl} alt="서경대학교 SEOKYEONG UNIVERSITY" className="logo-img" />
+                </div>
+                <div className="login-header-title">(서경대 학부 수강신청)</div>
+            </header>
+
+            {/* 2. 파란색 테두리의 메인 로그인 박스 */}
+            <main className="login-box">
+                {/* 로그인 입력 폼 영역*/}
+                <div className="login-form-area">
+                    {/* 입력 필드 */}
+                    <div className="input-group">
+                        <div className="input-row">
+                            <label className="input-label">아이디</label>
+                            <input type="text" className="input-field" />
+                        </div>
+                        <div className="input-row">
+                            <label className="input-label">비밀번호</label>
+                            <input type="password" className="input-field" />
+                        </div>
+                    </div>
+
+                    {/* 로그인 버튼  */}
+                    <img src={loginBtnUrl} alt="loginBtn" className="btn-img" />
+
+                </div>
+
+                {/* [학번찾기] */}
+                <div className="find-id-link">[학번찾기]</div>
+
+                {/* 박스 내부 빨간색 경고 문구 */}
+                <div className="internal-warning">
+                    <div className="warning-text">
+                        ※ 비밀번호는 포탈비밀번호와 동일합니다. 유의하시기 바랍니다.
+                    </div>
+                    <div className="warning-text">
+                        ※ 최초수강신청하는 신·편입생의 경우 반드시 포탈 로그인 후 <br/>
+                        이용하시기 바랍니다.
+                    </div>
+                </div>
+
+            </main>
+
+            {/* 3. 박스 외부 하단 빨간색 경고 문구 */}
+            <footer className="external-warning-container">
+                <div className="external-text">
+                    ※ 하나의 인터넷창으로만 수강신청 하실 수 있습니다. 유의하시기 바랍니다.
+                </div>
+
+                <div className="external-text">
+                    ※ wifi로 접속하여 스마트폰이나 노트북으로 수강신청시 통신이나 보안규약에<br />
+                    문제가 생겨 정상적인 수강신청 처리가 안될 수 있으니 PC에서 수강신청하시기<br />
+                    바랍니다.
+                </div>
+            </footer>
+
+        </div>
+    );
+}
+
+export default Login;
