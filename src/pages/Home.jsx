6860cd7022de04292801f82884a4c2a14481b006
ye-seob <jakalob103@naver.com>
2025-12-01 22:32:12 +0900

    :sparkles: feat(#8): 로그인 직후 홈 화면 UI 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import SideBar from "../components/SideBar.jsx";
-
 import "../styles/home.css";
-
+import InfoNotice from "../components/InfoNotice.jsx";
+import WarningNotice from "../components/WarningNotice.jsx";
+import TwoSectionLayout from "../components/Layout";
 
 const Home = () => {
+
     return (
         <div className="home-container">
-
             <SideBar/>
-            <div className="main-content-area">
-
-            </div>
-
+            <TwoSectionLayout
+            top={<InfoNotice/>}
+            bottom={<WarningNotice/>}
+            />
         </div>
     );
 };
