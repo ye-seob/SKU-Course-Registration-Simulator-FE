950b568d2bfc78e9d5348a39aa6970dd5d32b1c7
ye-seob <jakalob103@naver.com>
2025-12-01 18:56:41 +0900

    :sparkles: feat(#10): logout ui 구현

---
+++ src/components/Logout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/logout.css';
+
+
+const Logout = () => {
+    return (
+        <div className="logout-container">
+            <div className="logout-header">
+                <span className="door">
+                   ㅁ
+                </span>
+                <button className="logout-btn">
+                    로그아웃
+                </button>
+            </div>
+
+        </div>
+    );
+}
+
+export default  Logout;
