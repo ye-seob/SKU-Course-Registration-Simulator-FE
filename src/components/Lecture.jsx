45df27d69e5a1e7b9280726ab9a2ff1f3b469e99
ye-seob <jakalob103@naver.com>
2025-12-04 21:37:44 +0900

    :recycle: refactor(#17): 강의 리스트 테이블 깨짐 조정

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
 const Lecture = ({ lecture }) => {
-    return (
-        <tr className="course-row">
-            <td>
-                <button className="cart-btn">장바구니담기</button>
-            </td>
-            <td>{lecture.id}</td>
-            <td className="text-left">{lecture.name}</td>
-            <td>{lecture.courseId}</td>
-            <td>{lecture.section}</td>
-            <td>{lecture.type}</td>
-            <td>{lecture.credit}</td>
-            <td>{lecture.time}</td>
-            <td>{lecture.enrolled}</td>
-            <td>{lecture.limit}</td>
-            <td>{lecture.professor}</td>
-            <td>{lecture.evalMethod}</td>
-            <td>{lecture.week}</td>
-            <td>{lecture.rating}</td>
-            <td className="text-left">{lecture.desc}</td>
-            <td className="text-left">{lecture.schedule}</td>
-            <td>{lecture.competency}</td>
-        </tr>
-    );
+        return (
+            <tr className="course-row">
+                    <td>
+                            <button className="cart-btn">장바구니담기</button>
+                    </td>
+                    <td>{lecture.id}</td>
+                    <td className="text-left">{lecture.name}</td>
+                    <td>{lecture.courseId}</td>
+                    <td>{lecture.section}</td>
+                    <td>{lecture.type}</td>
+                    <td>{lecture.credit}</td>
+                    <td>{lecture.time}</td>
+                    <td>{lecture.enrolled}</td>
+                    <td>{lecture.limit}</td>
+                    <td>{lecture.professor}</td>
+                    <td>{lecture.evalMethod}</td>
+                    <td>{lecture.week}</td>
+                    <td>{lecture.rating}</td>
+                    <td className="text-left">{lecture.desc}</td>
+                    <td className="text-left">{lecture.schedule}</td>
+                    <td>{lecture.competency}</td>
+            </tr>
+        );
 };
 
 export default Lecture;
--- src/components/LectureList.jsx
+++ src/components/LectureList.jsx
@@ -6,7 +6,7 @@ const LectureList = () => {
     const lectureData = [
         {
         id: 1,
-        name: "객체지향프로그래밍",
+        name: "100세시대소크라테스헬스클럽",
         courseId: "CS1149",
         section: "03",
         type: "전심",
@@ -313,36 +313,39 @@ const LectureList = () => {
     }];
 
     return (<div className="lecture-wrapper">
-            <p className="lecture-title-text">[자학과 개설강좌]</p>
-            <div className="lecture-container">
-                <table className="list">
-                    <thead>
-                    <tr>
-                        <th className="col-cart">장바구니담기</th>
-                        <th className="col-no">No</th>
-                        <th className="col-name">교과목명</th>
-                        <th className="col-code">학수번호</th>
-                        <th className="col-class">분반</th>
-                        <th className="col-type">이수<br/>구분</th>
-                        <th className="col-credit">학점</th>
-                        <th className="col-time">시간</th>
-                        <th className="col-person">수강<br/>인원</th>
-                        <th className="col-limit">제한<br/>인원</th>
-                        <th className="col-prof">담당교수</th>
-                        <th className="col-eval">성적평가방법</th>
-                        <th className="col-week">학기주차</th>
-                        <th className="col-rate">강의평가평점<br/>(5점만점)</th>
-                        <th className="col-desc">강의설명</th>
-                        <th className="col-schedule">강의시간<br/>(주)주간,(야)야간</th>
-                        <th className="col-competency">역량구분</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {lectureData.map((lecture) => (<Lecture key={lecture.id} lecture={lecture}/>))}
-                    </tbody>
-                </table>
-            </div>
-        </div>);
+        <div className="lectureList-info-bar">
+            <span className="lecture-title-text">[자학과 개설강좌]</span>
+        </div>
+        <div className="lecture-table-container">
+            <table className="lecture-table">
+                <thead>
+
+                <tr>
+                    <th className="col-cart">장바구니담기</th>
+                    <th className="col-no">No</th>
+                    <th className="col-name">교과목명</th>
+                    <th className="col-code">학수번호</th>
+                    <th className="col-class">분반</th>
+                    <th className="col-type">이수<br/>구분</th>
+                    <th className="col-credit">학<br/>점</th>
+                    <th className="col-time">시<br/>간</th>
+                    <th className="col-person">수강<br/>인원</th>
+                    <th className="col-limit">제한<br/>인원</th>
+                    <th className="col-prof">담당교수</th>
+                    <th className="col-eval">성적평가방법</th>
+                    <th className="col-week">학기주차</th>
+                    <th className="col-rate">강의평가평점<br/>(5점만점)</th>
+                    <th className="col-desc">강의설명</th>
+                    <th className="col-schedule">강의시간<br/>(주)주간,(야)야간</th>
+                    <th className="col-competency">역량구분</th>
+                </tr>
+                </thead>
+                <tbody>
+                {lectureData.map((lecture) => (<Lecture key={lecture.id} lecture={lecture}/>))}
+                </tbody>
+            </table>
+        </div>
+    </div>);
 };
 
 export default LectureList;
