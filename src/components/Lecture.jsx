1e2e1ccc3eedb40d2ffcfa260387b896db7e309e
ye-seob <jakalob103@naver.com>
2025-12-06 03:32:28 +0900

    :sparkles: feat(#18): 강의 목록 조회 API 구현

--- src/components/Lecture.jsx
+++ src/components/Lecture.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import {useCart} from "../hooks/useCart.js";
 
-const Lecture = ({ lecture }) => {
-        return (
-            <tr className="course-row">
-                    <td>
-                            <button className="cart-btn">장바구니담기</button>
-                    </td>
-                    <td>{lecture.id}</td>
-                    <td className="text-left">{lecture.name}</td>
-                    <td>{lecture.courseId}</td>
-                    <td>{lecture.section}</td>
-                    <td>{lecture.type}</td>
-                    <td>{lecture.credit}</td>
-                    <td>{lecture.time}</td>
-                    <td>{lecture.enrolled}</td>
-                    <td>{lecture.limit}</td>
-                    <td>{lecture.professor}</td>
-                    <td>{lecture.evalMethod}</td>
-                    <td>{lecture.week}</td>
-                    <td>{lecture.rating}</td>
-                    <td className="text-left">{lecture.desc}</td>
-                    <td className="text-left">{lecture.schedule}</td>
-                    <td>{lecture.competency}</td>
-            </tr>
-        );
+
+const Lecture = ({lecture}) => {
+    const { addCartMutation } = useCart();
+
+    const handleAdd = () => {
+        if (window.confirm("선택한 과목을 담으시겠습니까?")) {
+            addCartMutation.mutate(lecture.id);
+        }
+    };
+
+    return (
+        <tr className="course-row">
+            <td>
+                <button className="cart-btn" onClick={handleAdd}>장바구니담기</button>
+            </td>
+            <td>{lecture.id}</td>
+            <td className="text-left">{lecture.lectureName}</td>
+            <td>{lecture.lectureCode}</td>
+            <td>{lecture.classNumber}</td>
+            <td>{lecture.type}</td>
+            <td>{lecture.credit}</td>
+            <td>{lecture.time}</td>
+            <td>{lecture.enrollment}</td>
+            <td>{lecture.capacity}</td>
+            <td>{lecture.professor}</td>
+            <td>{lecture.evalMethod}</td>
+            <td>{lecture.week}</td>
+            <td>{lecture.rating}</td>
+            <td className="text-left">{lecture.desc}</td>
+            <td className="text-left">{lecture.schedule}</td>
+            <td>{lecture.competency}</td>
+        </tr>
+    );
 };
 
 export default Lecture;
--- src/components/LectureList.jsx
+++ src/components/LectureList.jsx
@@ -1,351 +1,70 @@
 import React from "react";
 import "../styles/LectureList.css";
 import Lecture from "./Lecture.jsx";
+import {useLecture} from "../hooks/useLecture.js";
 
 const LectureList = () => {
-    const lectureData = [
-        {
-        id: 1,
-        name: "100세시대소크라테스헬스클럽",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 7,
-        name: "빅데이터소프트웨어",
-        courseId: "CS1403",
-        section: "01",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "유커열",
-        evalMethod: "",
-        week: "",
-        rating: "4.46",
-        desc: "(복-518)-컴퓨터시스템설계실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 1,
-        name: "객체지향프로그래밍",
-        courseId: "CS1149",
-        section: "03",
-        type: "전심",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0.00",
-        limit: "35.00",
-        professor: "김선희",
-        evalMethod: "",
-        week: "",
-        rating: "",
-        desc: "(복-617)-소프트웨어개발실습실",
-        schedule: "(주) 화 : 23, 24",
-        competency: ""
-    }, {
-        id: 8,
-        name: "알고리즘",
-        courseId: "CS101",
-        section: "01",
-        type: "전공",
-        credit: "3",
-        time: "3.0",
-        enrolled: "0",
-        limit: "30",
-        professor: "김교수",
-        evalMethod: "P/nP",
-        week: "",
-        rating: "",
-        desc: "A101",
-        schedule: "월1, 수3",
-        competency: ""
-    }];
+    const { lectureQuery } = useLecture();
+    const lectureData = lectureQuery.data || [];
 
-    return (<div className="lecture-wrapper">
-        <div className="lectureList-info-bar">
-            <span className="lecture-title-text">[자학과 개설강좌]</span>
-        </div>
-        <div className="lecture-table-container">
-            <table className="lecture-table">
-                <thead>
+    return (
+        <div className="lecture-wrapper">
+            <div className="lectureList-info-bar">
+                <span className="lecture-title-text">[자학과 개설강좌]</span>
+            </div>
 
-                <tr>
-                    <th className="col-cart">장바구니담기</th>
-                    <th className="col-no">No</th>
-                    <th className="col-name">교과목명</th>
-                    <th className="col-code">학수번호</th>
-                    <th className="col-class">분반</th>
-                    <th className="col-type">이수<br/>구분</th>
-                    <th className="col-credit">학<br/>점</th>
-                    <th className="col-time">시<br/>간</th>
-                    <th className="col-person">수강<br/>인원</th>
-                    <th className="col-limit">제한<br/>인원</th>
-                    <th className="col-prof">담당교수</th>
-                    <th className="col-eval">성적평가방법</th>
-                    <th className="col-week">학기주차</th>
-                    <th className="col-rate">강의평가평점<br/>(5점만점)</th>
-                    <th className="col-desc">강의설명</th>
-                    <th className="col-schedule">강의시간<br/>(주)주간,(야)야간</th>
-                    <th className="col-competency">역량구분</th>
-                </tr>
-                </thead>
-                <tbody>
-                {lectureData.map((lecture) => (<Lecture key={lecture.id} lecture={lecture}/>))}
-                </tbody>
-            </table>
+            <div className="lecture-table-container">
+                <table className="lecture-table">
+                    <colgroup>
+                        <col style={{ width: "110px" }} />  {/* 장바구니담기 */}
+                        <col style={{ width: "25px" }} />  {/* No */}
+                        <col style={{ width: "180px" }} /> {/* 교과목명 */}
+                        <col style={{ width: "70px" }} />  {/* 학수번호 */}
+                        <col style={{ width: "40px" }} />  {/* 분반 */}
+                        <col style={{ width: "50px" }} />  {/* 이수구분 */}
+                        <col style={{ width: "40px" }} />  {/* 학점 */}
+                        <col style={{ width: "40px" }} />  {/* 시간 */}
+                        <col style={{ width: "50px" }} />  {/* 수강인원 */}
+                        <col style={{ width: "50px" }} />  {/* 제한인원 */}
+                        <col style={{ width: "70px" }} />  {/* 담당교수 */}
+                        <col style={{ width: "70px" }} />  {/* 성적평가 */}
+                        <col style={{ width: "50px" }} />  {/* 학기주차 */}
+                        <col style={{ width: "60px" }} />  {/* 강의평가 */}
+                        <col style={{ width: "100px" }} /> {/* 강의실명 */}
+                        <col style={{ width: "100px" }} /> {/* 강의시간 */}
+                        <col style={{ width: "60px" }} />  {/* 역량구분 */}
+                    </colgroup>
+
+                    <thead>
+                    <tr>
+                        <th className="col-cart">장바구니담기</th>
+                        <th className="col-no">No</th>
+                        <th className="col-name">교과목명</th>
+                        <th className="col-code">학수번호</th>
+                        <th className="col-class">분반</th>
+                        <th className="col-type">이수<br/>구분</th>
+                        <th className="col-credit">학<br/>점</th>
+                        <th className="col-time">시<br/>간</th>
+                        <th className="col-person">수강<br/>인원</th>
+                        <th className="col-limit">제한<br/>인원</th>
+                        <th className="col-prof">담당교수</th>
+                        <th className="col-eval">성적평가방법</th>
+                        <th className="col-week">학기주차</th>
+                        <th className="col-rate">강의평가평점<br/>(5점만점)</th>
+                        <th className="col-desc">강의실명</th>
+                        <th className="col-schedule">강의시간<br/>(주)주간,(야)야간</th>
+                        <th className="col-competency">역량구분</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {lectureData.map((lecture) => (
+                        <Lecture key={lecture.id} lecture={lecture}/>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>);
+    );
 };
 
 export default LectureList;
