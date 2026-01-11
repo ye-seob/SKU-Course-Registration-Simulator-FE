c1a5237bb1791924e5811d2f701d41db593acd4d
ye-seob <jakalob103@naver.com>
2026-01-11 20:56:59 +0900

    :recycle: refactor(#39) : 버튼 클릭시 react 새로고침

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SideBar from "../components/SideBar.jsx";
 import "../styles/home.css";
 import TwoSectionLayout from "../components/Layout";
@@ -11,7 +11,7 @@ import useViewStore from "../store/viewStore.js";
 
 const Home = () => {
     const mode = useViewStore((state) => state.mode);
-
+    const [refreshKey, setRefreshKey] = useState(0);
     const isHome = mode === 'HOME';
 
     const topComponent = isHome ? <InfoNotice /> : <LectureList />;
@@ -19,8 +19,9 @@ const Home = () => {
 
     return (
             <div className="home-container">
-                <SideBar />
+                <SideBar  onRefresh={() => setRefreshKey(prev => prev + 1)} />
                 <TwoSectionLayout
+                    key={refreshKey}
                     top={topComponent}
                     bottom={bottomComponent}
                 />
