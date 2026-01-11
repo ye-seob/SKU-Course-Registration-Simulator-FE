b9e76966186d556eb2b1529e976f619c4c9f338b
ye-seob <jakalob103@naver.com>
2026-01-11 20:51:15 +0900

    :sparkles: feat(#39): 전역 응답 api 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -15,5 +15,28 @@ api.interceptors.request.use(
     },
     (error) => Promise.reject(error)
 );
+api.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+        if (!error.response) {
+            console.error("Network error:", error);
+            return Promise.reject({ message: "Network Error", originalError: error });
+        }
+
+        const { status, data } = error.response;
+
+        if (status === 403) {
+             alert("토큰이 만료되었습니다. 다시 로그인 해주세요.")
+             localStorage.removeItem('accessToken');
+             window.location.href = '/login';
+        }
+
+
+        console.error("API Error:", data?.message || error.message);
+
+        return Promise.reject(error);
+    }
+);
+
 
 export default api;
